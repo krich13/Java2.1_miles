a2dddba8602386f6a0eb710bae5436090d9dacb2
Christine Klimenchenko <klimenchenko.kri.ch@gmail.com>
2021-07-25 14:35:12 +0300

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,7 @@
+public class main {
+        public static void main(String[] args) {
+            int ticket_price = 2000;
+            int miles_amount = ticket_price / 20;
+            System.out.println(miles_amount);
+        }
+    }
